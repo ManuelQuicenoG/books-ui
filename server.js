3e5b3584cb2b406f8a772cdad291b929a10e3b6a
manuel.quiceno <manuel.quiceno@correo.icesi.edu.co>
2021-02-19 12:13:47 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('*', function (req, res) {
     const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
-});
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

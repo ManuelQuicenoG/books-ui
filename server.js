06ef1cd1d7f4162bca95e1f6a32053b062a61cfc
manuel.quiceno <manuel.quiceno@correo.icesi.edu.co>
2021-02-19 12:38:30 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontbooksmilo'));
+app.use(express.static(__dirname + '/dist/books-ui'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/frontbooksmilo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/books-ui/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
